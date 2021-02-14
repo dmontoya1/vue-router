53328e710c394f5504e769baf6dd6125a7ebd629
Daniel Montoya <dmontoya.lab@gmail.com>
2021-02-14 17:34:37 -0500

    Clase 34. Hooks Routers

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,15 +1,30 @@
 <template>
   <h1>This is a Game</h1>
   <p>{{ $route.params.name }}</p>
+
+  <button @click="load">Cargar</button>
 </template>
 
 <script>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 export default {
   name: "Game",
   setup() {
     const route = useRoute();
-    console.log(route.params.name);
+    const router = useRouter();
+    console.log(router);
+
+    const load = () => {
+      console.log("Ejecutando Petición");
+      setTimeout(() => {
+        console.log("Volver a la Home");
+        router.push("/");
+      }, 3000);
+    };
+
+    return {
+      load,
+    };
   },
 };
 </script>
