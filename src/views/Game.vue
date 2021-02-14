d4909c6b8603a1122c8399c95c76a774f3e548d6
Daniel Montoya <dmontoya.lab@gmail.com>
2021-02-14 17:29:14 -0500

    Clase 33. Rutas dinámicas

---
+++ src/views/Game.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>This is a Game</h1>
+  <p>{{ $route.params.name }}</p>
+</template>
+
+<script>
+import { useRoute } from "vue-router";
+export default {
+  name: "Game",
+  setup() {
+    const route = useRoute();
+    console.log(route.params.name);
+  },
+};
+</script>
+
+<style></style>
